4ed0882e6929c0e6ffb8fa6c7b19705378edd336
Robert Petzold <RobertP@ppedv.de>
2019-02-26 15:06:06 +0100

    HelloWorld, WhiskeyManager

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,55 @@ namespace HelloWorld
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private EventHandler _myEvent;
+        public event EventHandler MyEvent
+        {
+            add
+            {
+                _myEvent += value;
+            }
+            remove
+            {
+                _myEvent -= value;
+            }
+        }
+
+        bool _secondButtonCreated = false;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_secondButtonCreated)
+                return;
+
+            Button newButton = new Button();
+            newButton.HorizontalAlignment = HorizontalAlignment.Left;
+            newButton.VerticalAlignment = VerticalAlignment.Center;
+            newButton.Content = "Klick mich auch!";
+            newButton.Click += NewButton_ClickAsync;
+
+            mainGrid.Children.Add(newButton);
+            _secondButtonCreated = true;
+            
+        }
+
+        private async void NewButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog("Hello World!").ShowAsync();
+            this.Background = new SolidColorBrush(Colors.Aqua);
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in mainGrid.Children)
+            {
+                if(item is Button button)
+                {
+                    button.Click -= NewButton_ClickAsync;
+                }
+            }
         }
     }
 }
---
+++ WhiskeyManager/Whiskey.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiskeyManager
+{
+    class Whiskey : INotifyPropertyChanged
+    {
+        public enum WhiskeyType { SingleMalt, DoubleMalt, TripleMalt }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Snippet: propfull + Tab + Tab
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        private int _jahrgang;
+        public int Jahrgang
+        {
+            get { return _jahrgang; }
+            set
+            {
+                _jahrgang = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Jahrgang)));
+            }
+        }
+
+        private bool _leer;
+        public bool Leer
+        {
+            get { return _leer; }
+            set
+            {
+                _leer = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Leer)));
+            }
+        }
+
+        private WhiskeyType _type;
+        public WhiskeyType Type
+        {
+            get { return _type; }
+            set
+            {
+                _type = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Type)));
+            }
+        }
+
+    }
+}
